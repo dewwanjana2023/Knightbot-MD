68266b0a7f7ce69784ef6ea26b020a8cde30fa53
dewwanjana2023 <138287233+dewwanjana2023@users.noreply.github.com>
2025-03-01 14:17:58 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Dew',
   author: 'Bot',
-  botName: "Knight Bot",
+  botName: "Dew Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  ownerNumber: '94760683496', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
